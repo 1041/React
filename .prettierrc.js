109e5e5efe176f356493506d4430472c4b63ffbd
Toshikazu Kitani <toshigas@gmail.com>
2023-12-18 21:53:14 +0900

    コード整形を実施

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,22 +1,22 @@
 module.export = {
-    printWidth: 80,
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
-    trailingComma: "es5",
-    ovverrides: [
-        {
-            files: "*.test.js",
-            options: {
-                semi: false,
-            },
-        },
-        {
-            files: ["*.html", "legacy/**/*.js"],
-            options: {
-                singleQuote: false,
-                tabWidth: 2,
-            },
-        },
-    ],
+  printWidth: 80,
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+  trailingComma: "es5",
+  ovverrides: [
+    {
+      files: "*.test.js",
+      options: {
+        semi: false,
+      },
+    },
+    {
+      files: ["*.html", "legacy/**/*.js"],
+      options: {
+        singleQuote: false,
+        tabWidth: 2,
+      },
+    },
+  ],
 };
